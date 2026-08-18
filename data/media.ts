bb6b4f1581413679c79d3da8b7624cd079b3e313
akdi3679 <3kadiak679@gmail.com>
2026-08-18 07:47:17 +0200

    make all data on data folder and remove the hardcoded data

--- data/media.ts
+++ data/media.ts
@@ -9,4 +9,5 @@ export const cloudinaryConfig = {
 };
 
 // URL avec fl_attachment → force le téléchargement silencieux
-export const mediaKitUrl = `https://res.cloudinary.com/${cloudinaryConfig.cloudName}/raw/upload/fl_attachment:La-Viree-d-Hector-Media-Kit/${cloudinaryConfig.folders.mediaKit}/Media-Kit.pdf`;
+export const mediaKitUrl = `https://res.cloudinary.com/${cloudinaryConfig.cloudName}/image/upload/${cloudinaryConfig.folders.mediaKit}/Media-Kit.pdf`;
+
