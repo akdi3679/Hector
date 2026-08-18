adc01c1211236935cb3b7f4b116d4bb3b5d75c48
akdi3679 <3kadiak679@gmail.com>
2026-08-18 21:58:20 +0200

    add favicon

--- components/MediaStrip.tsx
+++ components/MediaStrip.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 
 interface Item { id: string; url: string; type: 'image' | 'gif' | 'video'; }
 
